bfee979cba0b5b8ec72e6fc89ad4d1fb719bf30c
Jonathan Groberg <jongroberg@gmail.com>
2024-01-16 15:29:33 -0500

    created a list element

---
+++ src/components/ListGroup.tsx
@@ -0,0 +1,12 @@
+function ListGroup() {
+  return (
+    <>
+      <h1 className="ps-3">List of items</h1>
+      <ul className="list-group">
+        <li className="list-group-item">Item 1</li>
+      </ul>
+    </>
+  );
+}
+
+export default ListGroup;
