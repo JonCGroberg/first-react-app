9238186908f96c0503c435538deaa1e439e42d1d
Jonathan Groberg <jongroberg@gmail.com>
2024-01-23 11:50:18 -0500

    editing task v0

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,48 +1,71 @@
-import { useState } from "react";
+import { MouseEvent, useState } from "react";
+import { PencilFill, Plus, Trash3Fill } from "react-bootstrap-icons";
+import "../App.css";
 
 function ListGroup() {
   const [tasks, setTasks] = useState([
-    "item1",
-    "item2",
-    "item3",
-    "item4",
-    "item5",
-    "item6",
+    "Do dishes",
+    "Do laundry",
+    "Take Bob to school",
+    "Calc III discusion assignment",
+    "Read 1 page of 12 Rules For Life",
+    "Do daily green square",
   ]);
-  const [newTask,setNewTask] = useState("")
+  const [newTask, setNewTask] = useState("");
 
   function handleDeleteTask(index: number) {
     setTasks(tasks.filter((_, i) => i !== index));
   }
 
-  function handleNewTask(){
-    setTasks([...tasks,newTask])
+  function handleNewTask(e: MouseEvent<HTMLButtonElement, MouseEvent>) {
+    e.preventDefault();
+    if (newTask != "") setTasks([...tasks, newTask]);
+    setNewTask("");
   }
+
+  function handleEditTask(e) {}
 
   return (
     <>
       <h1 className="">Todo</h1>
-      <div className=" input-group mb-3 shadow-lg">
+      <form className="input-group mb-3 shadow-lg">
         <input
           type="text"
           className=" form-control py-3"
           value={newTask}
           placeholder="Enter a task"
           onChange={(e) => setNewTask(e.target.value)}
-        />{" "}
-        <input type="button" value="Add" className="btn bg-success-subtle" onClick={()=>handleNewTask()} />
-      </div>
+        />
+        <button
+          type="submit"
+          className="btn btn-light shadow-sm border px-3"
+          onClick={(e) => handleNewTask(e)}
+        >
+          {" "}
+          <Plus></Plus>{" "}
+        </button>
+      </form>
       <ul className="list-group shadow">
         {tasks.map((item, index) => (
-          <li className="list-group-item py-3">
+          <li className="list-group-item hover py-3">
             <span className="">
               {item.charAt(0).toUpperCase() + item.substring(1)}
             </span>
-            <span className="float-end btn bg-danger-subtle m-0 p-0 px-1">
+            <span className="float-end m-0 p-0 px-1">
               <span
-                className="btn btn-close p-0 m-0"
+                className="btn p-0 m-0 px-1 ms-1"
+                onClick={() => handleEditTask(index)}
+              >
+                {" "}
+                <PencilFill></PencilFill>
+              </span>
+              <span
+                className="btn p-0 m-0 px-1 ms-1 text-danger"
                 onClick={() => handleDeleteTask(index)}
-              ></span>
+              >
+                {" "}
+                <Trash3Fill />
+              </span>
             </span>
           </li>
         ))}
